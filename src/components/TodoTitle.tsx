2a5d20060e82ac5a6ea4d939944f1c05d0a24976
amirrezash0n <shourvarziamirreza@gmail.com>
2026-09-16 09:57:37 +0330

    refactor(TodoTitle): make responsive and remove hardcoded spacing

--- src/components/TodoTitle.tsx
+++ src/components/TodoTitle.tsx
@@ -1,6 +1,6 @@
 export default function TodoTitle() {
   return (
-    <h1 className="text-3xl font-bold text-center mb-8 text-gray-800 dark:text-white">
+    <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 dark:text-white">
       Todo List ;)
     </h1>
   );
